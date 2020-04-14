884b3273b88f2e637c2827c5bd5f3fba44a64f3c
vrntsv <vomoran@gmail.com>
2020-04-14 18:44:19 +0300

    initial files

---
+++ app/load.php
@@ -0,0 +1,6 @@
+<?php
+    require_once 'core/models.php';
+    require_once 'core/views.php';
+    require_once 'core/controllers.php';
+    require_once 'core/route.php';
+    Route::start(); // запускаем маршрутизатор
